5b35e61bb6aa760c732b846826318cf879fa3d96
Tim Übelhör <t.uebelhoer@irt.rwth-aachen.de>
2020-08-26 08:00:11 +0200

    Propper relocatable CMake config files for downstream packages

--- cmake/gl3w-config.cmake
+++
@@ -1,9 +0,0 @@
-if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.8.0)
-	include(CMakeFindDependencyMacro)
-	find_dependency(OpenGL REQUIRED)
-endif()
-
-include("${CMAKE_CURRENT_LIST_DIR}/gl3w-targets.cmake")
-set(gl3w_INCLUDE_DIRS include)
-set(gl3w_LIBRARIES gl3w)
-
